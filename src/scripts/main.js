84623f41644efba03fab5d8b1244b611b0c32498
Maryan Mats <cholgyni@gmail.com>
2023-05-08 23:17:37 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.documentElement.classList.add('page--menu-open');
-  } else {
-    document.documentElement.classList.remove('page--menu-open');
-  }
-});
